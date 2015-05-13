bd40bcbd51ea44a77ac7f963e71c464bff2b49ea
amiliteev <amiliteev@pacificmetrics.com>
2015-05-12 17:14:26 -0700

    Added bower.json and hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+define([
+    'jquery',
+    'underscore'
+], function($, _) {
+    'use strict';
+
+    return {
+
+        say: function(param) {
+            console.log('hello ' + param);
+        }
+    };
+
+});
